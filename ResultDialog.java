3ed251ab0bbf3122e6d2ba46dd79a74485630402
JeonYongHak <162122203+JeonYongHak@users.noreply.github.com>
2024-06-06 14:57:03 +0900

    Update ResultDialog.java

--- ResultDialog.java
+++ ResultDialog.java
@@ -1,12 +1,1 @@
-package LiarGame;
 
-import javax.swing.*;
-
-public class ResultDialog {
-    public ResultDialog(String votedLiar, String actualLiar, boolean winner) {
-        String resultMessage = "투표된 라이어: " + votedLiar + "\n실제 라이어: " + actualLiar + "\n";
-        resultMessage += winner ? "시민의 승리!" : "라이어의 승리!";
-
-        JOptionPane.showMessageDialog(null, resultMessage, "게임 결과", JOptionPane.INFORMATION_MESSAGE);
-    }
-}
